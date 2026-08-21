2058df25e068bebea4cb463d934bcf70375c971e
CodeMakers-KR <codemakers.kr@gmail.com>
2026-08-21 13:49:09 +0900

    재귀조인 연습을 위한 임시 테이블

--- 문법소개.sql
+++ 문법소개.sql
@@ -1,3 +1,147 @@
+-- RECURSIVE JOIN
+--	MENU 임시테이블 만들기
+WITH MENU AS (
+	SELECT '1000' AS MENU_ID
+		 , '브랜드패션' AS MENU_NAME
+		 , NULL AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2000' AS MENU_ID
+		 , '패션의류/잡화/뷰티' AS MENU_NAME
+		 , NULL AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1100' AS MENU_ID
+		 , '브랜드의류' AS MENU_NAME
+		 , '1000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1200' AS MENU_ID
+		 , '브랜드잡화' AS MENU_NAME
+		 , '1000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1300' AS MENU_ID
+		 , '스포츠브랜드' AS MENU_NAME
+		 , '1000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2100' AS MENU_ID
+		 , '패션의류' AS MENU_NAME
+		 , '2000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2200' AS MENU_ID
+		 , '잡화' AS MENU_NAME
+		 , '2000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2300' AS MENU_ID
+		 , '뷰티' AS MENU_NAME
+		 , '2000' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1101' AS MENU_ID
+		 , '브랜드 여성의류' AS MENU_NAME
+		 , '1100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1102' AS MENU_ID
+		 , '브랜드 남성의류' AS MENU_NAME
+		 , '1100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1103' AS MENU_ID
+		 , '브랜드 캐쥬얼의류' AS MENU_NAME
+		 , '1100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1201' AS MENU_ID
+		 , '브랜드 잡화' AS MENU_NAME
+		 , '1200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1202' AS MENU_ID
+		 , '브랜드 쥬얼리/시계' AS MENU_NAME
+		 , '1200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1203' AS MENU_ID
+		 , '수입명품' AS MENU_NAME
+		 , '1200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1301' AS MENU_ID
+		 , '브랜드 아웃도어' AS MENU_NAME
+		 , '1300' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '1302' AS MENU_ID
+		 , '브랜드 스포츠패션' AS MENU_NAME
+		 , '1300' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2101' AS MENU_ID
+		 , '여성의류' AS MENU_NAME
+		 , '2100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2102' AS MENU_ID
+		 , '남성의류' AS MENU_NAME
+		 , '2100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2103' AS MENU_ID
+		 , '언더웨어' AS MENU_NAME
+		 , '2100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2104' AS MENU_ID
+		 , '유아동의류' AS MENU_NAME
+		 , '2100' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2201' AS MENU_ID
+		 , '신발' AS MENU_NAME
+		 , '2200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2202' AS MENU_ID
+		 , '가방/잡화' AS MENU_NAME
+		 , '2200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2203' AS MENU_ID
+		 , '유아동 신발/잡화' AS MENU_NAME
+		 , '2200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2203' AS MENU_ID
+		 , '쥬얼리/시계' AS MENU_NAME
+		 , '2200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2204' AS MENU_ID
+		 , '수입명품' AS MENU_NAME
+		 , '2200' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2301' AS MENU_ID
+		 , '화장품/향수' AS MENU_NAME
+		 , '2300' AS TOP_MENU_ID
+      FROM DUAL
+     UNION 
+    SELECT '2302' AS MENU_ID
+		 , '바디/헤어' AS MENU_NAME
+		 , '2300' AS TOP_MENU_ID
+      FROM DUAL
+)
+SELECT MENU_ID
+	 , MENU_NAME
+	 , TOP_MENU_ID
+  FROM MENU
+;
+
 -- ERD.
 -- 테이블 조인 + 그룹핑
 -- 직무명 별 수행중인 사원의 수를 조회한다.
